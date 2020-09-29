887b39a2449673cb152c00cbd6542ea5776baa1e
Joseguera <joseguera@uclaextension.edu>
2020-09-29 09:03:49 -0700

    react testing examples

---
+++ src/Card.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './Card';
+import renderer from 'react-test-renderer';
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+
+    ReactDOM.render(<Card />, div);
+
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+
+it('renders the UI as expected', () => {
+    const tree = renderer
+      .create(<Card title='Title' content= 'messages' />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();  
+});
